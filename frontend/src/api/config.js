5ff6fb408385897263a6d779ba30a48206b142a8
Jada Woody <JadaWoody@MacBook-Pro-16.local>
2020-11-17 13:23:25 -0500

    tuesdday1210

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -4,7 +4,7 @@ if( process.env.NODE_ENV === 'production' ) {
   if(process.env.REACT_APP_API){ 
     baseURL = `${process.env.REACT_APP_API}/api` //Netlify 
   } else {
-    baseURL = `/https://encompass-dev.herokuapp.com` //Heroku or 5000 
+    baseURL = `/api` //Heroku or 5000 
   }
 } else {
   baseURL = `http://localhost:5000/api` //3000
